6dc01618d552af5f28ea34f3ff68864750bb8b8d
Daphne Staruch <daphstr@gmail.com>
2023-08-14 16:18:14 -0400

    first scenario

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,13 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @input = params[:word].chars
+    @score = ''
+
+    @letters = ('a'..'z').to_a.sample(10)
+    # ['Cat', 'Dog', 'Bird'].include? 'Dog'
+    @input.each do |word|
+      @letters.all? { |letter| letter == word } ? @score = "true" : @score = "false"
+    end
   end
 end
